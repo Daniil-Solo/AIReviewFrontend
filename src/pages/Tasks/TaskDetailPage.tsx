e1ca71b8dc427c3174b45137e4cb6deea97eb67e
Daniil-Solo <daniil.solo1723@gmail.com>
2026-04-22 08:04:03 +0500

    add percentage of steps

--- src/pages/Tasks/TaskDetailPage.tsx
+++ src/pages/Tasks/TaskDetailPage.tsx
@@ -466,7 +466,7 @@ function TaskSolutionsTab({ taskId, workspaceId }: { taskId: number; workspaceId
           </Table.Thead>
           <Table.Tbody>
             {solutions?.map((solution) => {
-              const progress = (solution.steps.length / 6) * 100;
+              const progress = (solution.steps.length / 8) * 100;
               const showProgress = solution.status === 'AI_REVIEW' || solution.status === 'ERROR';
               return (
                 <Table.Tr key={solution.id}>
