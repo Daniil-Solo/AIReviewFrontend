94ea54560805c69d07da78eb520d91958e5078f9
Daniil-Solo <daniil.solo1723@gmail.com>
2026-04-23 09:05:16 +0500

    add dev version

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
 			? {
 					proxy: {
 						'/api': {
-							target: 'http://localhost:8000',
+							target: 'http://app:8000',
 							changeOrigin: true,
 						},
 					},
