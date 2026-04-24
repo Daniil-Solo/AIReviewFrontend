4735ba386f2af15ba7e0895f648893896d4df811
Daniil Solovjev <80649413+Daniil-Solo@users.noreply.github.com>
2026-04-24 06:45:08 +0500

    feature: docker, image publish
    * fix linters
    
    * add dev version
    
    * add configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
 	plugins: [react()],
-	server:
-		mode === 'development'
-			? {
-					proxy: {
-						'/api': {
-							target: 'http://localhost:8000',
-							changeOrigin: true,
-						},
-					},
-				}
-			: undefined,
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://app:8000',
+				changeOrigin: true,
+				logLevel: 'debug',
+			},
+		},
+	},
 }));
